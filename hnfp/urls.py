d95ffc68ec95dd101ff007198d4555f8d6fac238
David Pollard <pollardld@gmail.com>
2017-06-12 21:55:22 +0100

    start of modularized survey

--- hnfp/urls.py
+++ hnfp/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^home/', views.home),
+    url(r'^home', views.home),
     url(r'^survey', views.survey),
     url(r'^', views.index, name='index'),
 ]
