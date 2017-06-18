e277176f33aef86d1d83256a9ab6ed6228352854
David Pollard <pollardld@gmail.com>
2017-06-18 18:59:23 +0100

    add alert area

--- hnfp/static/hnfp/js/dashboard.js
+++ hnfp/static/hnfp/js/dashboard.js
@@ -1,7 +1,31 @@
-function getWeather() {
-// Todo openweather api request
-  axios.get('/get/server')
-    .then(function (response) {
+(function getWeather() {
+  var weatherDOM = $('#openweathermap'),
+      metric = false;
+  $.ajax({
+    type: 'GET',
+    url: '//api.openweathermap.org/data/2.5/weather?id=5847155&appid=9d7e64f21bfc706e679ffc6cd895b306',
+    dataType: 'jsonp',
+    success: function(data) {
+      console.log(data);
 
-    }
-}
+      if (metric === false) {
+      	// define temperature as fahrenheit
+      	var temperature = Math.round(((data.main.temp - 273.15) * 1.8) + 32) + '°F';
+      	var minTemperature = Math.round(((data.main.temp_min - 273.15) * 1.8) + 32) + '°F';
+      	var maxTemperature = Math.round(((data.main.temp_max - 273.15) * 1.8) + 32) + '°F';
+      } else {
+      	// define temperature as celsius
+      	var temperature = Math.round(data.main.temp - 273.15) + '°C';
+      	var minTemperature = Math.round(data.main.temp_min - 273.15) + '°C';
+      	var maxTemperature = Math.round(data.main.temp_max - 273.15) + '°C';
+      }
+      // TODO apply KO bindings
+      // set temperature
+      weatherDOM.html(temperature);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.warn(jqXHR + textStatus);
+      weatherDOM.html(errorThrown);
+  	}
+  });
+})();
