fee4a10190734bf9514ddd469add460faa6ae797
David Pollard <pollardld@gmail.com>
2017-06-26 18:09:47 -0700

    add darksky api

--- hnfp/static/hnfp/js/dashboard.js
+++ hnfp/static/hnfp/js/dashboard.js
@@ -1,6 +1,36 @@
+// Generic function to make an AJAX call
+var getJSON = function(dataURL, successCallback, errorCallback) {
+    // Return the $.ajax promise
+    return $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: dataURL,
+      success: successCallback,
+      error: errorCallback
+    });
+}
+
+$(document).ready(function() {
+  navigator.geolocation.getCurrentPosition(geolocate);
+
+  var marineURL = 'https://api.darksky.net/forecast/5dee4e51a4b71918ffe3ad18e83db386/';
+
+  function geolocate(pos) {
+    marineURL = '//api.darksky.net/forecast/5dee4e51a4b71918ffe3ad18e83db386/' + pos.coords;
+  }
+
+  getJSON(marineURL, loadMarine);
+
+  function loadMarine(data) {
+    console.log(data);
+    $('#marineforecast').html(data);
+  }
+});
+
 (function getWeather() {
   var weatherDOM = $('#openweathermap'),
       metric = false;
+
   $.ajax({
     type: 'GET',
     url: '//api.openweathermap.org/data/2.5/weather?id=5847155&appid=9d7e64f21bfc706e679ffc6cd895b306',
