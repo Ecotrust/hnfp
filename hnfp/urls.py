98abe36fe9b322c2ded572281653221105803c9e
David Pollard <pollardld@gmail.com>
2017-06-29 16:30:51 -0700

    survey updates

--- hnfp/urls.py
+++ hnfp/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^home', views.home),
-    url(r'^survey', views.survey),
-    url(r'^registered', views.registered),
-    url(r'^dashboard', views.dashboard),
-    url(r'^observations', views.observations),
-    url(r'^new_observation', views.new_observation),
+    url(r'^home/', views.home, name='home'),
+    url(r'^survey/', views.survey, name='survey'),
+    url(r'^registered/', views.registered, name='registered'),
+    url(r'^dashboard/', views.dashboard, name='dashboard'),
+    url(r'^observations/', views.observations, name='observations'),
+    url(r'^new_observation/', views.new_observation, name='new_observation'),
     url(r'^', views.home, name='index'),
 ]
