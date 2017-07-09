317f98e705905897f47ef668b0579a6996a25570
David Pollard <pollardld@gmail.com>
2017-07-08 19:02:24 -0700

    add marine conditions api

--- hnfp/static/hnfp/js/dashboard.js
+++ hnfp/static/hnfp/js/dashboard.js
@@ -1,47 +1,79 @@
 (function getWeather() {
-  var weatherDOM = $('#openweathermap'),
-      marineDOM = $('#marineforecast'),
-      metric = false;
+  var weatherDOM = $('#weatherforcast'),
+      marineDOM = $('#marineforecast');
 
   $.ajax({
     type: 'GET',
-    url: '//api.openweathermap.org/data/2.5/weather?id=5847155&appid=9d7e64f21bfc706e679ffc6cd895b306',
+    url: 'https://api.darksky.net/forecast/5dee4e51a4b71918ffe3ad18e83db386/58.1097,-135.4435',
     dataType: 'jsonp',
     success: function(data) {
-      console.log(data);
-
-      if (metric === false) {
-      	// define temperature as fahrenheit
-      	var temperature = Math.round(((data.main.temp - 273.15) * 1.8) + 32) + '°F';
-      	var minTemperature = Math.round(((data.main.temp_min - 273.15) * 1.8) + 32) + '°F';
-      	var maxTemperature = Math.round(((data.main.temp_max - 273.15) * 1.8) + 32) + '°F';
-      } else {
-      	// define temperature as celsius
-      	var temperature = Math.round(data.main.temp - 273.15) + '°C';
-      	var minTemperature = Math.round(data.main.temp_min - 273.15) + '°C';
-      	var maxTemperature = Math.round(data.main.temp_max - 273.15) + '°C';
+      weatherDOM.html(data.minutely.summary);
+      let current = data.currently;
+      let marineData = {
+        'nearest storm': current.nearestStormDistance,
+        'storm bearing': current.nearestStormBearing,
+        'wind speed': current.windSpeed,
+        'wind gust': current.windGust,
+        'wind bearing': current.windBearing,
+        'visibility': current.visibility,
+        'uv index': current.uvIndex
+      };
+      for (var prop in marineData) {
+        if (marineData.hasOwnProperty(prop)) {
+          console.log(prop + ': ' + marineData[prop]);
+          if (marineData[prop] !== undefined) {
+            marineDOM.append('<strong>' + prop + '</strong>: ' + marineData[prop] + '<br />').addClass('weather-block');
+          }
+        }
       }
-      // TODO apply KO bindings
-      // set temperature
-      weatherDOM.html(temperature);
     },
     error: function(jqXHR, textStatus, errorThrown) {
-      console.warn(jqXHR + textStatus);
-      weatherDOM.html(errorThrown);
-  	}
-  });
-
-  $.ajax({
-    type: 'GET',
-    url: 'https://api.darksky.net/forecast/5dee4e51a4b71918ffe3ad18e83db386/58.109190,-135.444947',
-    dataType: 'jsonp',
-    success: function(data) {
-      console.log(data);
-      marineDOM.html(data.currently.summary);
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      console.warn(jqXHR + textStatus);
       marineDOM.html(errorThrown);
   	}
   });
 })();
+
+Date.prototype.yyyymmdd = function() {
+  var mm = this.getMonth() + 1; // getMonth() is zero-based
+  var dd = this.getDate();
+  return [this.getFullYear(), (mm>9 ? '' : '0') + mm, (dd>9 ? '' : '0') + dd].join('');
+};
+
+Date.prototype.daysInFuture = function(days) {
+  if (days === undefined) {
+    var days = 0;
+  }
+  return new Date( date.getFullYear(), date.getMonth(), date.getDate() + days );
+};
+
+var date = new Date();
+
+var weather = {
+  tidesDOM: function() {
+    let tidesDOM = $('#tideforecast');
+    return tidesDOM
+  },
+  getTides: function() {
+    var date = new Date(),
+        tmrw = new Date( date.getFullYear(), date.getMonth(), date.getDate() + 1 ),
+        futureDate = date.daysInFuture(5).yyyymmdd(),
+        today = date.yyyymmdd(),
+        tmrw = tmrw.yyyymmdd();
+
+    $.ajax({
+      type: 'GET',
+      url:'https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date=' + today + '&end_date=' + futureDate + '&datum=MLLW&station=9452438&time_zone=lst_ldt&units=english&interval=hilo&format=json',
+      dataType: 'json',
+      success: function(data) {
+        JSON.parse(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.warn(jqXHR + textStatus);
+    	}
+    });
+  }
+};
+
+$(document).ready( function() {
+  weather.getTides();
+});
