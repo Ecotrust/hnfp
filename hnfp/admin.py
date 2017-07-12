05f8dbb01393ea11314e4c1c208647c49136f9d9
David Pollard <pollardld@gmail.com>
2017-07-11 21:21:36 -0700

    fix admin

--- hnfp/admin.py
+++ hnfp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-#from django.contrib.auth.admin import UserAdmin
-#from django.contrib.auth.models import User
-from hnfp.models import *
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Post, PublicManager, Question, Category, Survey, Response, AnswerText, AnswerRadio, AnswerSelect, AnswerInteger, AnswerSelectMultiple, AOI
 
 
 # Blog posts for forum
@@ -17,16 +17,18 @@ class PostAdmin(admin.ModelAdmin):
                   {'fields': ('title', 'slug', 'body',
                               'allow_comments', 'publish',)}),)
 
-# Register your models here.
+@admin.register(Question)
 class QuestionInline(admin.TabularInline):
 	model = Question
 	ordering = ('category',)
 	extra = 0
 
+@admin.register(Category)
 class CategoryInline(admin.TabularInline):
 	model = Category
 	extra = 0
 
+@admin.register(Survey)
 class SurveyAdmin(admin.ModelAdmin):
 	inlines = [CategoryInline, QuestionInline]
 
@@ -50,14 +52,9 @@ class AnswerSelectMultipleInline(AnswerBaseInline):
 class AnswerIntegerInline(AnswerBaseInline):
 	model= AnswerInteger
 
+@admin.register(Response)
 class ResponseAdmin(admin.ModelAdmin):
 	list_display = ('interview_uuid', 'interviewer', 'created')
 	inlines = [AnswerTextInline, AnswerRadioInline, AnswerSelectInline, AnswerSelectMultipleInline, AnswerIntegerInline]
 	# specifies the order as well as which fields to act on
 	readonly_fields = ('survey', 'created', 'updated', 'interview_uuid')
-
-#admin.site.register(Question, QuestionInline)
-#admin.site.register(Category, CategoryInline)
-admin.site.register(Survey, SurveyAdmin)
-
-admin.site.register(Response, ResponseAdmin)
