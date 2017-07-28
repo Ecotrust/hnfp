ee13dcaf60e37f1c57e7753edd0eaf96c8fe66ea
David Pollard <pollardld@gmail.com>
2017-07-27 17:48:17 -0700

    add blocks directory. add ecotrust logo

--- hnfp/project_settings.py
+++ hnfp/project_settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '-u*-d*&7j=c7a7&k5u6e61b4-t=d8ce^2k=jhox#cn8iy8m_%d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -41,6 +41,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ## remove for production
+    'livereload.middleware.LiveReloadScript'
 ]
 
 ROOT_URLCONF = 'marineplanner.urls'
@@ -153,6 +155,8 @@ COMPRESS_CACHE_BACKEND = 'default'
 COMPRESS_OFFLINE = True
 COMPRESS_OFFLINE_CONTEXT = {}
 COMPRESS_OFFLINE_MANIFEST = 'manifest.json'
+COMPRESS_REBUILD_TIMEOUT = 60000
+COMPRESS_MINT_DELAY = 40
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -160,6 +164,11 @@ STATICFILES_FINDERS = [
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
     'compressor.finders.CompressorFinder',
 ]
+
+KML_SIMPLIFY_TOLERANCE = 20 #meters
+KML_SIMPLIFY_TOLERANCE_DEGREES = 0.0002 # very roughly ~ 20 meters
+KML_EXTRUDER_HEIGHT = 100
+KML_ALTITUDEMODE_DEFAULT = 'absolute'
 
 ### Note: This MUST be set before importing project_settings, even though
 #       INSTALLED_APPS is the first thing project_settings sets.
@@ -172,28 +181,41 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.contenttypes',
+
     ### ADD DJANGO APPS ###
     'django.contrib.gis',
-    ### END ADD APPS ###
+    ### END ADD DJANGO APPS ###
+
+    ### REMOVE for production apps ##
+    'livereload',
+    ### END remove for prod ###
+
+    ### ADD APPS ###
     'marineplanner',
     'core',
     'compressor',
-    "import_export",
+    'import_export',
+    ### END ADD APPS ###
+
     ### BEGIN INSERTED INSTALLED APPS ###
-    "hnfp",
-    "features",
-    "manipulators",
-    "accounts",
-    "data_manager",
-    "visualize",
-    "nursery",
-    "drawing",
-    "rpc4django",
-    "django_comments_xtd",
-    "django_comments",
+    'hnfp',
+    'features',
+    'manipulators',
+    'accounts',
+    'data_manager',
+    'visualize',
+    'nursery',
+    'drawing',
+    'scenarios',
+    'analysistools',
+    'rpc4django',
+    'django_comments_xtd',
+    'django_comments',
+    'ckeditor',
     ### END INSERTED INSTALLED APPS ###
+
     ### BEGIN MISSING APPS ###
-    "captcha",
+    'captcha',
     'social.apps.django_app.default',
     'social_django',
     ### END MISSING APPS ###
@@ -214,6 +236,117 @@ try:
 except ImportError:
     pass
 
+# registration
+REGISTRATION_FORM_FIELDS = {
+    'first_and_last': True,
+    'preferred_name': True,
+    'username': True,
+    'email': True,
+    'password': True,
+    'confirm_password': True,
+    'captcha': True,
+}
+
+LOG_IN_WITH_EMAIL = False
+
+ADMIN_URL = "/admin/"
+
+# authentication
+
+
+SOCIAL_AUTH_LOGIN_OPTIONS = [
+    # 'Twitter',
+    # 'Facebook',
+    # 'Google',
+]
+
+
+SOCIAL_AUTH_NEW_USER_URL = '/account/?new=true&login=django'
+SOCIAL_AUTH_FACBEOOK_NEW_USER_URL = '/account/?new=true&login=facebook'
+SOCIAL_AUTH_GOOGLE_PLUS_NEW_USER_URL = '/account/?new=true&login=gplus'
+SOCIAL_AUTH_TWITTER_NEW_USER_URL = '/account/?new=true&login=twitter'
+
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/account/?login=django'
+SOCIAL_AUTH_GOOGLE_PLUS_LOGIN_REDIRECT_URL = '/account/?login=gplus'
+SOCIAL_AUTH_FACEBOOK_LOGIN_REDIRECT_URL = '/account/?login=facebook'
+SOCIAL_AUTH_TWITTER_LOGIN_REDIRECT_URL = '/account/?login=twitter'
+
+# SOCIAL_AUTH_GOOGLE_PLUS_KEY = ''
+# SOCIAL_AUTH_GOOGLE_PLUS_SECRET = ''
+# SOCIAL_AUTH_GOOGLE_PLUS_SCOPES = (
+#     'https://www.googleapis.com/auth/plus.login', # Minimum needed to login
+#     'https://www.googleapis.com/auth/plus.profile.emails.read', # emails
+# )
+
+SOCIAL_AUTH_FACEBOOK_KEY = ''
+SOCIAL_AUTH_FACEBOOK_SECRET = ''
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['public_profile,email']
+
+SOCIAL_AUTH_TWITTER_KEY = ''
+SOCIAL_AUTH_TWITTER_SECRET = ''
+
+# SOCIAL_AUTH_EMAIL_FORCE_EMAIL_VALIDATION = True
+SOCIAL_AUTH_EMAIL_VALIDATION_FUNCTION = 'accounts.pipeline.send_validation_email'
+SOCIAL_AUTH_EMAIL_VALIDATION_URL = '/account/validate'
+
+SOCIAL_AUTH_DISCONNECT_REDIRECT_URL = '/'
+
+# Our authentication pipeline
+SOCIAL_AUTH_PIPELINE = (
+    'accounts.pipeline.clean_session',
+
+    # Get the information we can about the user and return it in a simple
+    # format to create the user instance later. On some cases the details are
+    # already part of the auth response from the provider, but sometimes this
+    # could hit a provider API.
+    'social.pipeline.social_auth.social_details',
+
+    # Get the social uid from whichever service we're authing thru. The uid is
+    # the unique identifier of the given user in the provider.
+    'social.pipeline.social_auth.social_uid',
+
+    # Verifies that the current auth process is valid within the current
+    # project, this is were emails and domains whitelists are applied (if
+    # defined).
+    'social.pipeline.social_auth.auth_allowed',
+
+    # Checks if the current social-account is already associated in the site.
+    'social.pipeline.social_auth.social_user',
+
+    # Make up a username for this person, appends a random string at the end if
+    # there's any collision.
+    'social.pipeline.user.get_username',
+
+    # Confirm with the user that they really want to make an account, also
+    # make them enter an email address if they somehow didn't
+    'accounts.pipeline.confirm_account',
+
+    # Send a validation email to the user to verify its email address.
+    'social.pipeline.mail.mail_validation',
+
+    # Create a user account if we haven't found one yet.
+    'social.pipeline.user.create_user',
+
+    # Create the record that associated the social account with this user.
+    'social.pipeline.social_auth.associate_user',
+
+    # Populate the extra_data field in the social record with the values
+    # specified by settings (and the default ones like access_token, etc).
+    'social.pipeline.social_auth.load_extra_data',
+
+    # Update the user record with any changed info from the auth service.
+    'social.pipeline.user.user_details',
+
+    # Set up default django permission groups for new users.
+    'accounts.pipeline.set_user_permissions',
+
+    # Grab relevant information from the social provider (avatar)
+    'accounts.pipeline.get_social_details',
+
+    # 'social.pipeline.debug.debug',
+    'accounts.pipeline.clean_session',
+)
+
 RECAPTCHA_PUBLIC_KEY = '6Lc30ScUAAAAAE64HuxC_zXrUlEuZxIlZy-WFYf-'
 RECAPTCHA_PRIVATE_KEY = '6Lc30ScUAAAAAFq-pBBz161BKPMrRPvDajNuJu3G'
 
@@ -221,11 +354,35 @@ COMMENTS_APP = 'django_comments_xtd'
 COMMENTS_XTD_MAX_THREAD_LEVEL = 2
 COMMENTS_XTD_CONFIRM_EMAIL = True
 
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Full',
+    },
+    'custom': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Format', 'FontSize'],
+            ['Bold', 'Italic', 'Underline','Strike','Subscript','Superscript'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['Image','Table','HorizontalRule','SpecialChar'],
+            [ 'TextColor','BGColor' ],
+            ['Undo','Redo'],
+            ['RemoveFormat', 'Source']
+        ]
+    }
+}
+
 EMAIL_HOST = "smtp.mail.com"
 EMAIL_PORT = "587"
-EMAIL_HOST_USER = "alias@mail.com"
+EMAIL_HOST_USER = "dev@hoonahstewards.net"
 EMAIL_HOST_PASSWORD = "yourpassword"
-DEFAULT_FROM_EMAIL = "Helpdesk <helpdesk@yourdomain>"
+DEFAULT_FROM_EMAIL = "Dev <dev@hoonahstewards.net>"
+
+try:
+    from marineplanner.local_settings import *
+except Exception as e:
+    pass
 
 # This seems to help with some backward compatibility
 import django
