e9db667e4bccfb86b3d7c3926cd522b555c7cde6
David Pollard <pollardld@gmail.com>
2017-07-30 21:42:22 -0700

    update weather calls

--- hnfp/static/hnfp/js/dashboard.js
+++ hnfp/static/hnfp/js/dashboard.js
@@ -7,21 +7,39 @@
     url: 'https://api.darksky.net/forecast/5dee4e51a4b71918ffe3ad18e83db386/58.1097,-135.4435',
     dataType: 'jsonp',
     success: function(data) {
-      weatherDOM.html('<p>' + data.hourly.summary + '</p>');
       let current = data.currently;
+      let weatherData = {
+        'summary': current.currently,
+        // 'icon': current.icon,
+        'temperature (f)': current.temperature,
+        'precipitation type': current.precipType,
+        'precipitation probability (%)': current.precipProbability * 100,
+        'pressure (hPa)': current.pressure,
+        'dew point (f)': current.dewPoint,
+        'humidity (%)': current.humidity * 100,
+        'wind speed (m/s)': current.windSpeed,
+        'wind gust (m/s)': current.windGust,
+      }
+      for (let prop in weatherData) {
+        if (weatherData.hasOwnProperty(prop)) {
+          if (weatherData[prop] != undefined) {
+            weatherDOM.append('<div class="col s10 l12 offset-s1"><div class="col s8"><strong>' + prop + '</strong></div><div class="col s4 right-align">' + weatherData[prop] + '</div></div>').addClass('weather-block');
+          }
+        }
+      }
       let marineData = {
-        'nearest storm': current.nearestStormDistance,
-        'storm bearing': current.nearestStormBearing,
-        'wind speed': current.windSpeed,
-        'wind gust': current.windGust,
-        'wind bearing': current.windBearing,
-        'visibility': current.visibility,
+        'nearest storm (mi)': current.nearestStormDistance,
+        'storm bearing (°N)': current.nearestStormBearing,
+        'wind speed (mph)': current.windSpeed,
+        'wind gust (mph)': current.windGust,
+        'wind bearing (°N)': current.windBearing,
+        'visibility (mi)': current.visibility,
         'uv index': current.uvIndex
       };
-      for (var prop in marineData) {
+      for (let prop in marineData) {
         if (marineData.hasOwnProperty(prop)) {
-          if (marineData[prop] !== undefined) {
-            marineDOM.append('<div class="col s6"><div class="col s8 right-align"><strong>' + prop + '</strong>:</div><div class="col s4 left-align">' + marineData[prop] + '</div></div>').addClass('weather-block');
+          if (marineData[prop] != undefined) {
+            marineDOM.append('<div class="col s10 l12 offset-s1"><div class="col s8"><strong>' + prop + '</strong></div><div class="col s4 right-align">' + marineData[prop] + '</div></div>').addClass('weather-block');
           }
         }
       }
@@ -55,7 +73,7 @@ var weather = {
 
     $.ajax({
       type: 'GET',
-      url:'https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date=' + today + '&end_date=' + today + '&datum=MLLW&station=9452438&time_zone=lst_ldt&units=english&interval=hilo&format=json',
+      url:'https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date=today&datum=MLLW&station=9452438&time_zone=lst_ldt&units=english&interval=hilo&format=json',
       dataType: 'json',
       success: function(data) {
         data.predictions.forEach(function(el, i, arr) {
