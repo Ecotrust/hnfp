dcac2271b87e92609873cd62e377c66c55e807e7
David Pollard <pollardld@gmail.com>
2017-08-09 16:38:36 -0700

    work on observation entry submit

--- hnfp/static/hnfp/js/observations.js
+++ hnfp/static/hnfp/js/observations.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
   $('.collapsible').collapsible();
-  $('.timepicker').pickatime({
-    default: 'now', // Set default time: 'now', '1:30AM', '16:30'
-    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
-    twelvehour: false, // Use AM/PM or 24-hour format
-    donetext: 'Okay', // text for done-button
-    cleartext: 'Clear', // text for clear-button
-    canceltext: 'Cancel', // Text for cancel-button
-    autoclose: true, // automatic close timepicker
-    ampmclickable: true, // make AM PM clickable
-    aftershow: function(){} //Function for after opening timepicker
-  });
-
 });
 
 var observations = {
@@ -25,14 +13,33 @@ var observations = {
             //debugger;
         }
     }).done(function() {
-
+      $('.timepicker').pickatime({
+        default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+        fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+        donetext: 'Okay', // text for done-button
+        cleartext: 'Clear', // text for clear-button
+        autoclose: true, // automatic close timepicker
+        ampmclickable: true, // make AM PM clickable
+        aftershow: function(){} //Function for after opening timepicker
+      });
+      $('#drawing-form').submit(function(e) {
+        e.preventDefault();
+        observations.addNew(e.target);
+      })
     });
   },
-  addNew: function(el) {
+  addNew: function(form) {
+    console.log(form);
     return $.ajax({
-      url: 'hnfp/new_observation',
-    }).done(function(data) {
-      $( '#new-observation' ).html(data);
-    });
+      type: 'POST',
+      url: '/observation/add/',
+      data: $(form).serialize(),
+      success: function(data) {
+        console.log(data);
+      },
+      error: function (result) {
+        //debugger;
+      }
+    })
   }
 };
--- hnfp/views.py
+++ hnfp/views.py
@@ -11,24 +11,23 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model, authenticate, login
 
-from accounts.actions import apply_user_permissions, send_password_reset_email,\
-    send_social_auth_provider_login_email, generate_username
+from accounts.actions import apply_user_permissions, send_password_reset_email, send_social_auth_provider_login_email, generate_username
 from accounts.models import EmailVerification, UserData, PasswordDictionary
-from accounts.forms import SignUpForm, ForgotPasswordForm,\
-    ResetPasswordForm, SocialAccountConfirmForm, LogInForm, UserDetailForm, \
-    ChangePasswordForm
+from accounts.forms import SignUpForm, ForgotPasswordForm, ResetPasswordForm, SocialAccountConfirmForm, LogInForm, UserDetailForm, ChangePasswordForm
 from accounts.signals import user_post_save
 from nursery.view_helpers import decorate_view
 
 
-from accounts.widgets import BSLeftIconTextInput, BSLeftIconPasswordInput,\
-    BSLeftIconEmailInput
+from accounts.widgets import BSLeftIconTextInput, BSLeftIconPasswordInput, BSLeftIconEmailInput
 from django.core.exceptions import ValidationError
 from captcha.fields import ReCaptchaField
 
 # survey
 from hnfp.models import Question, Survey, Category, Post, PublicManager, JobOpportunity
 from hnfp.forms import ResponseForm
+
+# observation
+from hnfp.models import Observation, ObservationLocation, ObservationCategory
 
 def index(request):
     template = loader.get_template('hnfp/index.html')
@@ -47,14 +46,18 @@ def home(request):
     return HttpResponse(template.render(context, request))
 
 def observation_add(request):
-    User = get_user_model()
-
     if request.method == 'POST':
-        observation_category = request.POST['first_name']
-        observation_type = request.POST['last_name']
-        email = request.POST['email']
-        password = request.POST['password']
-        username = email
+        observation_category = request.POST['observation_category']
+        observation_type = request.POST['observation_type']
+        observation_tally = request.POST['observation_tally']
+        observation_photo = request.POST['observation_photo']
+        comments = request.POST['comments']
+        observation_date = request.POST['observation_date']
+        user = User.get_username()
+
+        new_obj = Observation.objects.create(**kwargs);
+
+        return HttpResponse(form, content_type="application/json")
 
 
 def registering(request):
