79023c4e023fb1c8a5c998b221216c2e33a61cc2
David Pollard <pollardld@gmail.com>
2017-08-10 14:14:16 -0700

    add hoonahstewards.net to allowed hosts

--- hnfp/project_settings.py
+++ hnfp/project_settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '-u*-d*&7j=c7a7&k5u6e61b4-t=d8ce^2k=jhox#cn8iy8m_%d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '*.hoonahstewards.net']
+ALLOWED_HOSTS = ['localhost', '*.hoonahstewards.net', 'hoonahstewards']
 
 
 # Application definition
