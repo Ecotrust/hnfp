6909534b914ab62ea9bb571045c06c0dc3191cdb
David Pollard <pollardld@gmail.com>
2017-10-03 13:49:20 -0700

    fixes to the service worker

--- hnfp/templates/sw.js
+++ hnfp/templates/sw.js
@@ -37,8 +37,11 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fromCache(event.request).catch(fromServer(event.request)));
-  event.waitUntil(update(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
 
 self.addEventListener('sync', function(event) {
@@ -68,27 +71,3 @@ self.addEventListener('notificationclick', function(event) {
     new WindowClient('/dashboard/');
   } */
 });
-
-function update(request) {
-  //this is where we call the server to get the newest version of the
-  //file to use the next time we show view
-  return caches.open(cacheName).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
-
-function fromCache(request) {
-  //we pull files from the cache first thing so we can show them fast
-  return caches.open(cacheName).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
-
-function fromServer(request){
-  //this is the fallback if it is not in the cahche to go to the server and get it
-  return fetch(request).then(function(response){ return response})
-}
