d3523a204b7ea667b7d484e094015bff7609e35e
David Pollard <pollardld@gmail.com>
2017-10-03 12:07:20 -0700

    bring back service worker fetch caching

--- hnfp/templates/sw.js
+++ hnfp/templates/sw.js
@@ -36,13 +36,13 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
-/* self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
   );
-}); */
+});
 
 self.addEventListener('sync', function(event) {
   self.registration.showNotification("Sync event fired!");
