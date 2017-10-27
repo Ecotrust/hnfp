9d43dc005b34d00efa6a2995baf7e360265d465b
David Pollard <pollardld@gmail.com>
2017-10-27 14:23:33 -0700

    improve performance

--- hnfp/static/hnfp/js/app.hnfp.js
+++ hnfp/static/hnfp/js/app.hnfp.js
@@ -2,3 +2,17 @@ $(document).ready( function() {
   $('.parallax').parallax();
   $('.button-collapse').sideNav();
 });
+
+/*
+  ***
+  lazy loading images
+    img with data-src attribute will get lazy loaded
+    the src for these images are set after page load
+*/
+
+[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+	img.setAttribute('src', img.getAttribute('data-src'));
+	img.onload = function() {
+		img.removeAttribute('data-src');
+	};
+});
