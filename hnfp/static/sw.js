4a7bb5c1e67a6975ed59133e15405883f50ed6c8
David Pollard <pollardld@gmail.com>
2018-05-16 17:44:42 -0700

    SW compile for push

--- hnfp/static/sw.js
+++ hnfp/static/sw.js
@@ -11,7 +11,7 @@ if (workbox) {
     // cache names
     workbox.core.setCacheNameDetails({
       prefix: 'hnfp',
-      suffix: 'v3.1'
+      suffix: 'v3.2'
     });
 
     // JSON post requests off network add to background sync queue
